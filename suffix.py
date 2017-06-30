bbda678d23de4ae6683ef4957fe0fada9eb151b6
Mihajlo Matković <mihajlo.matkovic99@gmail.com>
2017-06-30 17:01:21 +0200

    Add files via upload

---
+++ suffix.py
@@ -0,0 +1,24 @@
+rec1 = "peva"
+rec2 = "beogradski"
+rec3 = "spavanje"
+
+rec1_osnova=rec1[:3]
+print(rec1_osnova)
+
+rec1_sufiks=rec1[3:]
+print(rec1_sufiks)
+
+
+rec2_osnova=rec2[:7]   # -2,-5
+print(rec2_osnova)
+
+rec2_sufiks=rec2[7:] #   -2:
+print(rec2_sufiks)
+
+
+rec3_osnova=rec3[:4] 
+print(rec3_osnova)
+
+rec3_sufiks=rec3[4:]
+print(rec3_sufiks)
+
