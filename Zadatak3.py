bed414803abaf02199c2ceda70e929898a3948e8
Mihajlo Matković <mihajlo.matkovic99@gmail.com>
2017-07-01 17:40:25 +0200

    Add files via upload

---
+++ Zadatak3.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jul  1 17:25:35 2017
+
+@author: Win 7
+"""
+
+petnica = {"A": ["ANT", "AHL", "IST", "DIZ", "DRH", "LIN", "PSI"], "B": ["IO", "BMD", "EBH", "HEM", "GEO"], "C": ["AST", "FIZ", "MAT", "PFE", "RAČ", "TEH"]}
+
+print(petnica["A"]) 
